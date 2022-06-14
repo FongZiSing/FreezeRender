216cbf1514eb6a85d1c0453404da21ab2deee45a
FongZiSing <1052054788@qq.com>
2022-06-14 20:28:19 +0800

    [Feat] basic ray tracing.

--- FreezeRender/Sources/Engine/Renderer/RayTacing/RayTracingRenderer.cpp
+++ FreezeRender/Sources/Engine/Renderer/RayTacing/RayTracingRenderer.cpp
@@ -4,87 +4,100 @@
 
 
 
-using namespace Shading;
-
 RayTracingRenderer::RayTracingRenderer()
-    : width(2)
-    , height(2)
-    , scene(2, 2)
-    , primitiveRay(2, 2)
-    , bRegeneratePrimitiveRay(false)
+	: width(2)
+	, height(2)
+	, scene(2, 2)
+	, depth(2, 2)
+	, primitiveRay(2, 2)
+	, bRegeneratePrimitiveRay(false)
 {
 }
 
 void RayTracingRenderer::ScreenResize(int inWidth, int inHeight)
 {
-    width = inWidth;
-    height = inHeight;
+	width = inWidth;
+	height = inHeight;
 
-    scene.Resize(inWidth, inHeight);
-    primitiveRay.Resize(inWidth, inHeight);
-    bRegeneratePrimitiveRay = true;
+	scene.Resize(inWidth, inHeight);
+	depth.Resize(inWidth, inHeight);
+	primitiveRay.Reallocate(inWidth, inHeight);
+	bRegeneratePrimitiveRay = true;
 }
 
 ColorRenderTarget& RayTracingRenderer::Render(const RenderWorld* Scene)
 {
-    const Camera& viewBuffer = Scene->render.cameras[0];
-    const Array<Meshlet>& meshletBuffer = Scene->render.meshlets;
-    const Array<PointLight>& lightBuffer = Scene->render.pointlights;
+	const Camera& viewBuffer = Scene->render.cameras[0];
+	const Array<Meshlet>& meshletBuffer = Scene->render.meshlets;
+	const Array<PointLight>& lightBuffer = Scene->render.pointlights;
 
-    if (bRegeneratePrimitiveRay)
-    {
-        int c = 0;
-        float scale = std::tan(viewBuffer.halfFOV);
-        float aspectRatio = viewBuffer.data.aspectRatio;
-        for (int h = 0; h < height; ++h)
-        {
-            for (int w = 0; w < width; ++w)
-            {
-                float x = (2.f * (w + 0.5f) / (float)width - 1.f) * aspectRatio * scale;
-                float y = (1 - 2 * (h + 0.5f) / (float)height) * scale;
-                primitiveRay.SetPixel(c++, Ray { viewBuffer.data.location, Vector3(x, y, -1).Normalize() });
-            }
-        }
-        bRegeneratePrimitiveRay = false;
-    }
+	if (bRegeneratePrimitiveRay)
+	{
+		int c = 0;
+		float scale = std::tan(viewBuffer.halfFOV);
+		float aspectRatio = viewBuffer.data.aspectRatio;
+		for (int h = 0; h < height; ++h)
+		{
+			for (int w = 0; w < width; ++w)
+			{
+				float x = (2.f * (w + 0.5f) / (float)width - 1.f) * aspectRatio * scale;
+				float y = (1 - 2 * (h + 0.5f) / (float)height) * scale;
+				primitiveRay[c++] = { viewBuffer.data.location, Vector3(x, y, -1).Normalize() };
+			}
+		}
+		bRegeneratePrimitiveRay = false;
+	}
 
-    scene.Clear();
-    ParallelFor(0u, (unsigned int)width * height, [&](const unsigned int& worklistIndex)
-    {
+	scene.Clear();
+	depth.Clear();
+	ParallelFor(0u, (unsigned int)width * height, [&](const unsigned int& worklistIndex)
+	{
 		const Matrix& view = viewBuffer.view;
-        Ray& ray = primitiveRay.GetPixel(worklistIndex);
+		Ray& ray = primitiveRay[worklistIndex];
 
-        for (auto& perMeshlet : meshletBuffer)
-        {
-            branch_unlikely if (!perMeshlet.IsValid())
-            {
-                continue;
-            }
+		for (auto& perMeshlet : meshletBuffer)
+		{
+			branch_unlikely if (!perMeshlet.IsValid())
+			{
+				continue;
+			}
 
-            AMeshlet& meshlet = perMeshlet.data;
-            const Matrix mv = view * meshlet.transform;
+			AMeshlet& meshlet = perMeshlet.data;
+			const Matrix mv = view * meshlet.transform;
 
-            for (Meshlet::Iterator It = perMeshlet.CreateIterator(); It; ++It)
-            {
-                // Init triangle.
-                ShadingTriangle triangle = It.Assembly();
+			for (Meshlet::Iterator It = perMeshlet.CreateIterator(); It; ++It)
+			{
+				// Init triangle.
+				ShadingTriangle triangle = It.Assembly();
 
-                // Model-View-Projection.
-                triangle.vertices[0].viewspace.position = mv * triangle.vertices[0].viewspace.position;
-                triangle.vertices[1].viewspace.position = mv * triangle.vertices[1].viewspace.position;
-                triangle.vertices[2].viewspace.position = mv * triangle.vertices[2].viewspace.position;
+				// Model-View-Projection.
+				triangle.vertices[0].viewspace.position = mv * triangle.vertices[0].viewspace.position;
+				triangle.vertices[1].viewspace.position = mv * triangle.vertices[1].viewspace.position;
+				triangle.vertices[2].viewspace.position = mv * triangle.vertices[2].viewspace.position;
 
-                float neark = std::numeric_limits<float>::max();
-                HitResult result = ray.Intersect(triangle);
-                if (result == HitStatus::Success)
-                {
-                    neark = result.t;
-                    scene.SetPixel(worklistIndex, Color::Blue);
-                    break;
-                }
-            }
-        }
-    });
-    return scene;
+				float neark = std::numeric_limits<float>::max();
+				HitResult result = ray.Intersect(triangle);
+				if (result == HitStatus::Success)
+				{
+					neark = result.t;
+
+					const float rayLength = (ray.direction * neark).Length();
+
+					if (depth.GetPixel(worklistIndex) > rayLength)
+					{
+						depth.SetPixel(worklistIndex, rayLength);
+						const float& beta = result.beta;
+						const float& gamma = result.gamma;
+						const float& alpha = 1 - beta - gamma;
+						Vector2 uv = triangle.vertices[0].localspace.uv * alpha
+									+ triangle.vertices[1].localspace.uv * beta
+									+ triangle.vertices[2].localspace.uv * gamma;
+						scene.SetPixel(worklistIndex, triangle.material->diffuse.sampler(uv));
+					}
+				}
+			}
+		}
+	});
+	return scene;
 
 }
